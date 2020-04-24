093a5b26a2d70fa7bab1a6ac2f591a1bf7beb5cb
Ratan <Ratan@Ratan-PC>
2020-04-25 04:23:14 +0530

    Initial commit .Create spring boot project and configure the application properties

---
+++ onlinebookstore1/src/main/java/in/risithratan/onlinebookstore1/OnlineBookStore1Application.java
@@ -0,0 +1,13 @@
+package in.risithratan.onlinebookstore1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStore1Application.class, args);
+	}
+
+}
